67c803e20184250aa44ffa9fde3e08b56175c18d
uvaGANESHKUMAR <35688981+uvaGANESHKUMAR@users.noreply.github.com>
2018-02-05 15:58:11 +0530

    uva1.c

---
+++ uva1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int i,a,b=0;
+	printf("enter the number");
+	scanf("%d",&a);
+	for(i=0;i<=a;i++)
+	{
+		b=b+i;
+		
+	}
+	printf("the value is%d",b);
+	return 0;
+}
