208a8bdcc876f764b429e53da1f4a9bd6a2af3bc
uvaGANESHKUMAR <35688981+uvaGANESHKUMAR@users.noreply.github.com>
2018-02-19 16:41:45 +0530

    uva9.c

---
+++ uva9.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void)
+{
+   int a,i,b=5,ans=1,c=0;
+   scanf("%d",&a);
+   if(a==0 && a<=-1)
+   {
+   	printf("%d",c);
+   }
+   else
+   {
+	for(i=1;i<=b;i++)
+	{
+		ans=a*i;
+		printf("%d ",ans);
+	}
+   }
+   return 0;
