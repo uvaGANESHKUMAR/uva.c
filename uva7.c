d93d9edacf795c8e3c5535061900edb3ce70bca6
uvaGANESHKUMAR <35688981+uvaGANESHKUMAR@users.noreply.github.com>
2018-02-19 16:07:04 +0530

    uva7.c

---
+++ uva7.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int a,b,i,n,r;
+printf("enter the intervals");
+scanf("%d%d",&a,&b);
+printf("armstrong numbers between two intervals");
+for(i=1;i<=b;i++)
+{
+i=1;
+ s=0
+while(i=0)
+{
+r=n%10;
+ s=s=r*r*r;n=n\10;
+{
+ if(n==s)
+
+}
+}
+}
+
+ printf("%d\n",i);
+return();
+}
