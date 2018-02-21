6ce68bc13d4ad2baeb7c6a784088c531417b28ad
uvaGANESHKUMAR <35688981+uvaGANESHKUMAR@users.noreply.github.com>
2018-02-21 16:04:22 +0530

    uva10.c

---
+++ 1uva10.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,l;
+float ap;
+printf("enter the values for a,b,c");
+scanf("%d%d%d",&a,&b,&c);
+l=a+((b-1)*c);
+ap=(s/2)*(c+1);
+printf("the value is :",ap);
+return 0;
+}
