d1eb7bfaad4cdfc106806032f96a0596652e5e81
uvaGANESHKUMAR <35688981+uvaGANESHKUMAR@users.noreply.github.com>
2018-03-02 13:35:44 +0530

    uva32.c

---
+++ uva32.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main
+{
+int b,i;
+char a[50];
+printf("enter the word");
+scanf("%c",&c);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]==' ')
+count++;
+return 0;
+}
