90825a9e1f9243a6d8dcb2fa9ef71da0f1cf7b7b
uvaGANESHKUMAR <35688981+uvaGANESHKUMAR@users.noreply.github.com>
2018-03-09 14:11:45 +0530

    1uva44.c

---
+++ 1uva44.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int n;
+  clrscr();
+	printf("enter the number:");
+	scanf("%d",&n);
+	if(n>=1 && n<=10)
+	{
+		printf("\n yes");
+	}
+	else
+	{
+		printf("\n no");
+	}
+	return 0;
+  getch();
+  }
+  
