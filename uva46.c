c30701bb39f54ad8831c21c77f3918fc8beb3de5
uvaGANESHKUMAR <35688981+uvaGANESHKUMAR@users.noreply.github.com>
2018-03-14 15:47:41 +0530

    uva46.c

---
+++ uva46.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+int main(void) 
+{
+	int num;
+  clrscr();
+	printf("enter the number:");
+	scanf("%d",&num);
+	int result=num+1;
+	printf("\n%d",result);
+	return 0;
+  getch();
+}
