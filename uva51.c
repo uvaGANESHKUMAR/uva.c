0ada582d237dd5f154a6ad48110d58f004eb8a98
uvaGANESHKUMAR <35688981+uvaGANESHKUMAR@users.noreply.github.com>
2018-03-16 15:53:54 +0530

    uva51.c

---
+++ uva51.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+include <conio.h>
+int main(void) 
+{
+	int num,rem,rev=0;
+  clrscr()l;
+	printf("enter the number:");
+	scanf("%d",&num);
+	while(num!=0)
+	{
+		rem=num%10;
+		rev=(rev*10)+rem;
+		num=num/10;
+	}
+	num=rev;
+	while(num!=0)
+	{
+		rem=num%10;
+		printf("%d ",rem);
+		num=num/10;
+	}
+	return 0;
+  getch();
